8f91ace5ba0d9c4bdde158dad48fd2ed1db93768
Sidharth-Penta <pentasidharth1234@gmail.com>
2025-11-05 12:47:25 +0530

    Initial commit

---
+++ R/age.R
@@ -0,0 +1,15 @@
+#' Age
+#'
+#' @param a first argument
+#'
+#' @returns text including age
+#' @export
+#'
+#' @examples
+#' age(12)
+#' age(20)
+#'
+age <- function(a)
+{
+  paste0("I am ", a, " years old.")
+}
